c48e9bdf1af8a49217d559735d81c13a00e5df49
nickgarfield <nick@feather.id>
2020-05-13 13:24:54 -0500

    Introduce credential type constants

--- test/resources/credentials.spec.js
+++ test/resources/credentials.spec.js
@@ -1,17 +1,18 @@
 "use strict";
 
 const feather = require("../../testUtils").getSpyableFeather();
+const credentialTypes = require("../../lib/credentialTypes");
 
 const expect = require("chai").expect;
 
 describe("credentials resource", function() {
   it("[create] should create a credential", function() {
-    feather.credentials.create("foo", "foo@bar.com", "baz", "quz");
+    feather.credentials.create("email", "foo@bar.com", "baz", "quz");
     expect(feather._gateway.LAST_REQUEST).to.deep.equal({
       method: "POST",
       path: "/credentials",
       data: {
-        type: "foo",
+        type: "email",
         email: "foo@bar.com",
         username: "baz",
         password: "quz"
@@ -37,57 +38,107 @@ describe("credentials resource", function() {
     }).to.throw(/Type must be a string/);
   });
 
+  it("[create] should throw error if type is invalid", function() {
+    expect(() => {
+      feather.credentials.create("foo", null, null, null);
+    }).to.throw(/Invalid credential type: foo/);
+
+    expect(() => {
+      feather.credentials.create(credentialTypes.EMAIL, null, null, null);
+    }).to.not.throw();
+  });
+
   it("[create] should throw error if email is not a string", function() {
     expect(() => {
-      feather.credentials.create("foo", true, "bar", "baz");
+      feather.credentials.create(credentialTypes.EMAIL, true, "bar", "baz");
     }).to.throw(/Email must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", 123, "bar", "baz");
+      feather.credentials.create(credentialTypes.EMAIL, 123, "bar", "baz");
     }).to.throw(/Email must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", {}, "bar", "baz");
+      feather.credentials.create(credentialTypes.EMAIL, {}, "bar", "baz");
     }).to.throw(/Email must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", null, "bar", "baz");
+      feather.credentials.create(credentialTypes.EMAIL, null, "bar", "baz");
     }).to.not.throw();
   });
 
   it("[create] should throw error if username is not a string", function() {
     expect(() => {
-      feather.credentials.create("foo", "bar", true, "baz");
+      feather.credentials.create(
+        credentialTypes.USERNAME_PASSWORD,
+        "bar",
+        true,
+        "baz"
+      );
     }).to.throw(/Username must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", 123, "baz");
+      feather.credentials.create(
+        credentialTypes.USERNAME_PASSWORD,
+        "bar",
+        123,
+        "baz"
+      );
     }).to.throw(/Username must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", {}, "baz");
+      feather.credentials.create(
+        credentialTypes.USERNAME_PASSWORD,
+        "bar",
+        {},
+        "baz"
+      );
     }).to.throw(/Username must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", null, "baz");
+      feather.credentials.create(
+        credentialTypes.USERNAME_PASSWORD,
+        "bar",
+        null,
+        "baz"
+      );
     }).to.not.throw();
   });
 
   it("[create] should throw error if password is not a string", function() {
     expect(() => {
-      feather.credentials.create("foo", "bar", "baz", true);
+      feather.credentials.create(
+        credentialTypes.EMAIL_PASSWORD,
+        "bar",
+        "baz",
+        true
+      );
     }).to.throw(/Password must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", "baz", 123);
+      feather.credentials.create(
+        credentialTypes.EMAIL_PASSWORD,
+        "bar",
+        "baz",
+        123
+      );
     }).to.throw(/Password must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", "baz", {});
+      feather.credentials.create(
+        credentialTypes.EMAIL_PASSWORD,
+        "bar",
+        "baz",
+        {}
+      );
     }).to.throw(/Password must be a string/);
 
     expect(() => {
-      feather.credentials.create("foo", "bar", "baz", null);
+      feather.credentials.create(
+        credentialTypes.EMAIL_PASSWORD,
+        "bar",
+        "baz",
+        null
+      );
     }).to.not.throw();
   });
 
